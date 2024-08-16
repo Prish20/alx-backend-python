432a1cdf8bc585b9e85c395b0c60cd09b0f33fcd
Prish20 <prish@students.uonbi.ac.ke>
2024-08-17 01:13:39 +0300

    Task 5 fixes

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
-from typing import List
-
-"""
-This module provides a function to sum a list of floats.
-"""
-
-
-"""
-    Calculate the sum of a list of floats.
-
-    Args:
-        input_list (List[float]): A list containing float numbers.
-
-    Returns:
-        float: The sum of all float numbers in the list.
-    """
+""" Complex types - list of floats """
+from typing import Callable, Iterator, Union, Optional, List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """
+    Takes a list input_list of floats as argument
+    returns their sum as a float.
+    """
+
     return sum(input_list)
