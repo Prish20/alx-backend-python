54340bcf89fb1419cbaf406bf05c39d9b082e9ff
Prish20 <contact@christadriansanya.me>
2025-06-01 22:50:26 +0300

    Add search functionality to Conversation and Message viewsets

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
@@ -6,6 +6,8 @@ from .serializers import ConversationSerializer, MessageSerializer
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__username']
 
     def create(self, request, *args, **kwargs):
         participants = request.data.get('participants')
@@ -22,6 +24,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['message_body']
 
     def create(self, request, *args, **kwargs):
         conversation_id = request.data.get('conversation')
