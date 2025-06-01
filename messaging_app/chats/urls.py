5019f3dbd29bf85029ff906e2455b1047a3c7505
Prish20 <contact@christadriansanya.me>
2025-06-01 22:58:52 +0300

    Add nested routers import and API authentication URLs

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_nested import routers as nested_routers
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
