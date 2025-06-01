60719cdfed7444878429358089c94b70e03ed8fa
Prish20 <contact@christadriansanya.me>
2025-06-01 21:30:21 +0300

    Enhance unit tests for access_nested_map function with additional exception handling

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,7 +10,7 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-    Test the access_nested_map function.
+    Test class for the access_nested_map function.
     """
 
     @parameterized.expand([
@@ -27,3 +27,18 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path),
             expected
         )
+
+    @parameterized.expand([
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'"),
+    ])
+    def test_access_nested_map_exception(self, nested_map: dict,
+                                         path: tuple,
+                                         expected_message: str) -> None:
+        """
+        Test that access_nested_map raises
+        a KeyError with the expected message.
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), expected_message)
